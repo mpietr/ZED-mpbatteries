a5d3da06b1a1d0443203a9b3e3beff357a0f5400
Maria Pietras <pietrasmaria02@gmail.com>
2024-12-08 20:42:27 +0100

    add raport

--- app/app.R
+++ app/app.R
@@ -5,50 +5,41 @@ library(ggplot2)
 
 data <- read.csv('mp_batteries.csv')
 
-# Define UI
 ui <- fluidPage(
-  titlePanel("Interactive Density Plot"),
-  
   sidebarLayout(
     sidebarPanel(
-      # Dropdown for column selection
       selectInput(
         inputId = "selected_column",
-        label = "Choose a Column:",
-        choices = names(data %>% select(where(is.numeric)))  # Numeric columns only
+        label = "Choose an attribute:",
+        choices = names(data %>% select(where(is.numeric))),
+        selected = "Average.Voltage"
       )
     ),
-    
     mainPanel(
-      plotOutput("densityPlot") # Output the density plot
+      plotOutput("densityPlot")
     )
   )
 )
 
-# Define Server
 server <- function(input, output) {
   
-  # Reactive data for the selected column
   reactive_data <- reactive({
-    req(input$selected_column)  # Ensure a column is selected
+    req(input$selected_column)
     data %>%
       select(input$selected_column) %>%
       pivot_longer(cols = everything(), names_to = "name", values_to = "value")
   })
   
-  # Render Density Plot
   output$densityPlot <- renderPlot({
     reactive_data() %>%
       ggplot(aes(x = value)) +
-      geom_density(fill = "blue", alpha = 0.5) +
+      geom_density(fill = "darkblue", alpha = 0.25) +
       labs(
         title = paste("Density Plot for", input$selected_column),
         x = "Value",
         y = "Density"
-      ) +
-      theme_minimal()
+      ) + theme_minimal()
   })
 }
 
-# Run the App
 shinyApp(ui, server)
